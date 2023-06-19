e219f7bb932fe347a957ac6ca05b3749b1afee74
brenman60 <83514481+brenman60@users.noreply.github.com>
2023-06-19 15:20:08 -0400

    Added ScreenSlice Functionality

--- Safety Steps/Assets/Scripts/Obstacles/ScreenSlice.cs
+++ Safety Steps/Assets/Scripts/Obstacles/ScreenSlice.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class ScreenSlice : MonoBehaviour
 {
+    public float moveSpeed = 2.5f;
     public float timer = 2.5f;
+
+    private void Start()
+    {
+        StartCoroutine(MoveIntoPosition());
+    }
+
+    IEnumerator MoveIntoPosition()
+    {
+        bool top = transform.position.y == 10 ? true : false;
+        float newY = top ? 5 : -5;
+        Vector3 newPosition = new Vector3(transform.position.x, newY);
+        while (transform.position != newPosition) 
+        {
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * moveSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+    }
 
     private void Update()
     {
